5ea13956a6e6ec9e38aba569c5bd09e816c7b293
Diegotpereira <diegoteixeirapereira@hotmail.com>
2021-02-16 09:34:39 -0300

    alteração no arquivo environments adicionado chave firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDuVeDaOM9vALGi4xN7EAmJu1n8NbAeWgw",
+    authDomain: "whatsappclonemastersix.firebaseapp.com",
+    databaseURL: "https://whatsappclonemastersix.firebaseio.com",
+    projectId: "whatsappclonemastersix",
+    storageBucket: "whatsappclonemastersix.appspot.com",
+    messagingSenderId: "265712287603",
+    appId: "1:265712287603:web:d96b18e06d1133fde48f06",
+    measurementId: "G-FPX7V6NYQX"
+  }
 };
 
 /*
