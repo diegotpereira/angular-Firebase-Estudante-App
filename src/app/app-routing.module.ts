af406317147f2f7b5ccaf3938d15ba3f2f5961f6
Diegotpereira <diegoteixeirapereira@hotmail.com>
2021-02-16 09:42:26 -0300

    alteração no arquivo adicionado rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
+import {  AddEstudanteComponent  } from './add-estudante/add-estudante.component';
+import { EstudanteListComponent } from './estudante-list/estudante-list.component';
+import { EditEstudanteComponent } from './edit-estudante/edit-estudante.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'registro-estudante', pathMatch: 'full' },
+  { path: ' registro-estudante', component: EstudanteListComponent },
+  { path: ' view-estudantes', component: EstudanteListComponent },
+  { path: 'edit-estudante/:id', component: EditEstudanteComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
