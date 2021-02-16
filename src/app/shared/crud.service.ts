ac8ddec1283efba6b2f27ccd86bc9f8733e3fd35
Diegotpereira <diegoteixeirapereira@hotmail.com>
2021-02-16 09:33:56 -0300

    criação do arquivo crud service da aplicação

---
+++ src/app/shared/crud.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { Estudante } from '../shared/estudante';
+import { AngularFireDatabase, AngularFireList, AngularFireObject } from '@angular/fire/database';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CrudService {
+
+  estudantesRef: AngularFireList<any>;
+  estudanteRef: AngularFireObject<any>;
+
+  constructor(private db: AngularFireDatabase) { }
+
+  // Criar Estudante
+  AddEStudante(estudante: Estudante){
+    this.estudantesRef.push({
+      nome: estudante.nome,
+      sobrenome: estudante.sobrenome,
+      email: estudante.email,
+      telefone: estudante.telefone
+    })
+  }
+  GetEstudante(id: string){
+    this.estudanteRef = this.db.object('estudante/lista/' + id);
+    return this.estudanteRef;
+  }
+  UpdateEstudante(estudante: Estudante){
+    this.estudanteRef.update({
+      nome: estudante.nome,
+      sobrenome: estudante.sobrenome,
+      email: estudante.email,
+      telefone: estudante.telefone
+    })
+  }
+  DeleteEstudante(id: string){
+    this.estudanteRef = this.db.object('estudantes-lista/' +id);
+    this.estudanteRef.remove();
+  }
+}
