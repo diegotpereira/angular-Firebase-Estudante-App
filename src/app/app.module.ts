410cde86f1ceafe032184c07352a48d92f49a730
Diegotpereira <diegoteixeirapereira@hotmail.com>
2021-02-16 09:35:15 -0300

    alteração no arquivo adicionado imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { environment } from '../environments/environment';
+import { AddEstudanteComponent } from './add-estudante/add-estudante.component';
+import { EditEstudanteComponent } from './edit-estudante/edit-estudante.component';
+import { EstudanteListComponent } from './estudante-list/estudante-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddEstudanteComponent,
+    EditEstudanteComponent,
+    EstudanteListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
